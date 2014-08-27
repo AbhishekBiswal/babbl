9f91d404fc0a342a62b5001da75046a4e22d0192
Abhishek Biswal <abhishekbiswal@live.com>
2014-08-28 02:32:21 +0530

    added wsgi file

--- babbl/controllers.py
+++ babbl/controllers.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, request, Response
 from flask import render_template, url_for, redirect, send_from_directory
 
+from models import db, Posts, Rooms, Comments
+
 from babbl import app
 
 # app controllers
@@ -10,9 +12,59 @@ from babbl import app
 def index():
 	return render_template('index.html')
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
+@app.route('/<slug>')
+def home(slug):
+	room = Rooms.query.filter_by(slug=slug).first()
+	if room is None:
+		return "404. Room doesn't Exist"
+	return render_template('home.html', pageTitle="Home", slug=slug, room=room, db=db, Posts=Posts, Rooms=Rooms)
+
+@app.route('/new/<id>/<to>')
+def new(id, to):
+	to = "/"+to
+	return render_template('new.html', pageTitle="New Post", id=id, to=to)
+
+@app.route('/new/sub', methods=['POST'])
+def newSub():
+	type = request.form["post"]
+	id = request.form["id"]
+	ip = request.remote_addr
+	if type == "short":
+		content = request.form["content"]
+		color = request.form["color"].lower()
+		if content is None:
+			return redirect("/new")
+		if len(content) <= 5:
+			return "Insufficient Content. Go <a href='/new'>Back</a>"
+		newPost = Posts("short", "", content, ip, id, color)
+		db.session.add(newPost)
+		db.session.commit()
+		getId = Posts.query.filter_by(post_content=content, ip=ip).first()
+		if getId is None:
+			return "Error. Please Try again"
+		return redirect("/p/"+str(getId.id))
+	return 0
+
+@app.route('/p/<id>')
+def postPage(id):
+	post = Posts.query.filter_by(id=id).first()
+	room = Rooms.query.filter_by(id=post.room).first()
+	return render_template("post.html", post=post, db=db, Rooms=Rooms, Posts=Posts, Comments=Comments, room=room)
+
+@app.route("/p/comment", methods=['POST'])
+def comment():
+	comment = request.form['comment']
+	pid = request.form['pid']
+	if pid is None:
+		return "error"
+	if len(comment) == 0:
+		return "Comment too short"
+	newComment = Comments(comment, pid)
+	post = Posts.query.filter_by(id=pid).first()
+	post.comments = post.comments + 1
+	db.session.add(newComment)
+	db.session.commit()
+	return redirect("/p/"+str(pid))
 
 # special file handlers
 @app.route('/favicon.ico')
--- babbl/models.py
+++ babbl/models.py
@@ -2,10 +2,60 @@
 
 """
 import datetime
-from babbl.core import db
+from shuffl.core import db
 
 class User(db.DynamicDocument):
 	created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
 	email = db.StringField(max_length=255, required=True, unique=True)
 
 """
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Posts(db.Model):
+	__tablename__ = 'posts'
+	id = db.Column(db.Integer, primary_key=True)
+	post_type = db.Column(db.String(300))
+	post_title = db.Column(db.String(300))
+	post_content = db.Column(db.String(300))
+	ip = db.Column(db.String(300))
+	when = db.Column(db.String(300))
+	room = db.Column(db.Integer)
+	comments = db.Column(db.Integer)
+	hidden = db.Column(db.Integer)
+	color = db.Column(db.String(300))
+
+	def __init__(self, post_type, post_title, post_content, ip, room, color):
+		self.post_type = post_type
+		self.post_title = post_title
+		self.post_content = post_content
+		self.ip = ip
+		self.room = room
+		self.comments = 0
+		self.hidden = 0
+		self.color = color
+
+class Rooms(db.Model):
+	__tablename__ = 'rooms'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(300))
+	slug = db.Column(db.String(300))
+	desc = db.Column(db.String(300))
+
+	def __init__(self, name, slug, desc):
+		self.name = name
+		self.slug = slug
+		self.desc = desc
+
+class Comments(db.Model):
+	__tablename__ = 'comments'
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(300))
+	p_id = db.Column(db.Integer)
+	when = db.Column(db.String(300))
+
+	def __init__(self, content, p_id):
+		self.content = content
+		self.p_id = p_id
