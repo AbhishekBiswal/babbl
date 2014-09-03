26699f5c3c8539ad9d0997c3a423ed550ce63c67
Abhishek Biswal <abhishekbiswal@live.com>
2014-09-03 21:14:33 +0530

    Minor fixes.

--- babbl/static/js/scripts.js
+++ babbl/static/js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
 	$("a").click(function(){
-		$(".loading").show();
+		var href = $(this).attr("href");
+		if (href.indexOf('#') == -1)
+		{
+			$(".loading").show();
+		}
 	})
 
 })
